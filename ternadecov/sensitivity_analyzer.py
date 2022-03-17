942cb7a66ebcb67e701addadbc424f1618173ef3
Nikolas Barkas <nikolas.barkas@outlook.com>
2022-03-17 11:31:06 -0400

    sensitivity class cleanup

--- ternadecov/sensitivity_analyzer.py
+++ ternadecov/sensitivity_analyzer.py
@@ -4,28 +4,21 @@ from ternadecov.stats_helpers import *
 from ternadecov.deconvolution_plotter import *
 from ternadecov.parametrization import *
 
+import logging
+
 
 class SensitivityAnalyzer:
     """Container class for static methods pertaining to parameter sensitivity analysis"""
 
     @staticmethod
     def evaluate_deconvolution(
-        sc_anndata,
-        bulk_anndata,
+        dataset_param: DeconvolutionDatasetParametrization,
         hyperparameters: TimeRegularizedDeconvolutionModelParametrization,
         trajectory_hyperparameters: TimeRegularizedDeconvolutionGPParametrization,
         datatype_param: DeconvolutionDatatypeParametrization,
         n_iters=1_000,
     ):
         """Evaluate deconvolution with specified parametrization"""
-
-        # Make the dataset
-        dataset_param = DeconvolutionDatasetParametrization(
-            sc_anndata=sc_anndata,
-            sc_celltype_col="Subclustering_reduced",
-            bulk_anndata=bulk_anndata,
-            bulk_time_col="dpi_time",
-        )
 
         dataset = DeconvolutionDataset(
             parametrization=dataset_param, types=datatype_param,
@@ -57,34 +50,60 @@ class SensitivityAnalyzer:
     @staticmethod
     def scan_parameter(
         parameter,
-        start,
-        end,
-        num,
         sc_anndata,
         bulk_anndata,
         datatype_param,
         parameter_type="model",
+        parameter_variable_type="continuous",
+        start=None,
+        end=None,
+        num=None,
+        discrete_values=None,
         n_iters=10_000,
     ):
         """Scan the defined parameter with values in the specified range and save results"""
         results = {}
 
-        for i, v in enumerate(np.linspace(start, end, num)):
-            print(f"Evaluating with {parameter} = {v} ...")
+        if parameter_variable_type == "continuous":
+            assert start is not None
+            assert end is not None
+            assert num is not None
+            param_values = np.linspace(start, end, num)
+        elif parameter_variable_type == "discrete":
+            assert discrete_values is not None
+            param_values = discrete_values
+        else:
+            raise ValueError(
+                f"Unknown parameter_variable_type: {parameter_variable_type}"
+            )
 
+        # evaluation loop
+        for v in param_values:
+            logging.info(f"Evaluating with {parameter} = {v} ...")
+
+            # Preparing parameters
+            dataset_param = DeconvolutionDatasetParametrization(
+                sc_anndata=sc_anndata,
+                sc_celltype_col="Subclustering_reduced",
+                bulk_anndata=bulk_anndata,
+                bulk_time_col="dpi_time",
+            )
             model_param = TimeRegularizedDeconvolutionModelParametrization()
             trajectory_param = TimeRegularizedDeconvolutionGPParametrization()
 
+            # Modifying parameters
             if parameter_type == "model":
                 setattr(model_param, parameter, v)
             elif parameter_type == "trajectory":
                 setattr(trajectory_param, parameter, v)
+            elif parameter_type == "dataset":
+                setattr(dataset_param, parameter, v)
             else:
-                raise ValueError()
+                raise ValueError(f"Unknown parameter_type: {parameter_type}")
 
+            # Evaluating
             results[v] = SensitivityAnalyzer.evaluate_deconvolution(
-                sc_anndata=sc_anndata,
-                bulk_anndata=bulk_anndata,
+                dataset_param=dataset_param,
                 hyperparameters=model_param,
                 trajectory_hyperparameters=trajectory_param,
                 n_iters=n_iters,
