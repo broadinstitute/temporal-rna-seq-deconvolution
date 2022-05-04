7a46d9c1344c86c0c6fa658d1c341dad8e99f578
Nikolas Barkas <nikolas.barkas@outlook.com>
2022-05-04 18:00:19 +0000

    doc updates

--- ternadecov/sensitivity_analyzer.py
+++ ternadecov/sensitivity_analyzer.py
@@ -1,5 +1,4 @@
-"""Automated Sensitivity analysis"""
-
+"""Class for automated sensitivity analysis"""
 
 import logging
 import typing
@@ -8,7 +7,6 @@ import matplotlib
 import numpy as np
 import pyro
 import copy
-
 from ternadecov.parametrization import (
     DeconvolutionDatasetParametrization,
     TimeRegularizedDeconvolutionModelParametrization,
@@ -28,15 +26,22 @@ class SensitivityAnalyzer:
         hyperparameters: TimeRegularizedDeconvolutionModelParametrization,
         trajectory_hyperparameters: TimeRegularizedDeconvolutionGPParametrization,
         datatype_param: DeconvolutionDatatypeParametrization,
-        n_iters=1_000,
+        n_iters=10_000,
     ):
-        """Evaluate deconvolution with specified parametrization"""
+        """Evaluate deconvolution with specified parametrization
+        
+        :param dataset_param: Dataset parametrization
+        :param hyperparameters: Hyperparameters for TimeRegularizedDeconvolutionModel
+        :param trajectory_hyperparameters: Trajecotry hyperparameters for TimeRegularizedDeconvolutionModel
+        :param datatype_param: a DeconvolutionDatatypeParametrization
+        :param n_iters: Numver of iterations to run
+        
+        :return: Composition trajectories
+        """
 
         dataset = DeconvolutionDataset(
             parametrization=dataset_param, types=datatype_param,
         )
-
-        print(f"sc_anndata shape {dataset_param.sc_anndata.shape}")
 
         # Make the deconvolution model
         model = TimeRegularizedDeconvolutionModel(
@@ -55,7 +60,6 @@ class SensitivityAnalyzer:
             clear_param_store=True,
         )
 
-        # TODO: Save more information here
         traj = model.population_proportion_model.get_composition_trajectories(
             model.dataset, n_intervals=100
         )
@@ -64,7 +68,7 @@ class SensitivityAnalyzer:
 
     @staticmethod
     def scan_parameter(
-        parameter,
+        parameter: str,
         dataset_param: DeconvolutionDatasetParametrization,
         datatype_param,
         parameter_type="model",
@@ -77,7 +81,22 @@ class SensitivityAnalyzer:
         trajectory_param: TimeRegularizedDeconvolutionGPParametrization = None,
         n_iters=10_000,
     ):
-        """Scan the defined parameter with values in the specified range and save results"""
+        """Scan the defined parameter with values in the specified range and save results, performing deconvolution for each value
+        
+        :param parameter: name of parameter to scan
+        :param dataset_param: dataset parametrization to use
+        :param datatype_param: datatype parametrization to use
+        :param parameter_type: type of parameter ('model', 'trajectory' or 'dataset')
+        :param parameter_variable_type: variable type of parameter ('discrete' or 'continous')
+        :param start: start value, for continous variables
+        :param end: end value, for continous variables
+        :param num: number of values in the interval, for continous variables
+        :param discrete_values: list of discrete values, for discrete variables
+        :param model_param: Model parameters (which are modified as above)
+        :param traject_param: Trajectory parameters (which are modified as above)
+        
+        :return: dictionary of results
+        """
         results = {}
 
         dataset_param = copy.deepcopy(dataset_param)
@@ -127,7 +146,14 @@ class SensitivityAnalyzer:
 
     @staticmethod
     def plot_scan_trajectories(results, variable):
-        """Plot the results of scan_parameter"""
+        """Plot the results of scan_parameter
+        
+        :param results: scan results from scan_parameter()
+        :param variable: variable to plot
+        
+        :return: matplotlib axes
+        
+        """
 
         r = results[variable]
 
@@ -148,3 +174,5 @@ class SensitivityAnalyzer:
             cur_ax.set_xlabel("Time")
 
         fig.tight_layout()
+
+        return ax
